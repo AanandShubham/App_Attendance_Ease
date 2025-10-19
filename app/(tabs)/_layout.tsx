357edf3de2b5083531245b0e72c10b238113a987
AanandShubham <www.subham.anand@gmail.com>
2025-10-20 03:41:44 +0530

    tab color and icons design done

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,43 @@
 import { Tabs } from "expo-router"
+import { Ionicons } from '@expo/vector-icons';
 
 const TabsLayout = () => {
     return (
-        <Tabs>
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarStyle: {
+                    backgroundColor: '#3A87BD', // 💡 Your color here
+                    borderTopWidth: 0,
+                    elevation: 0,
+                },
+                tabBarActiveTintColor: '#0D1164',
+                tabBarInactiveTintColor: '#fff',
+            }}
+
+        >
             <Tabs.Screen
                 name="home"
-                options={{ title: "Home", headerShown: false }}
+                options={{
+                    title: "Home",
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="home" color={color} size={size} />
+                    ),
+
+                }}
+
             />
-        
+
             <Tabs.Screen
                 name="settings"
-                options={{ title: "Settings", headerShown: false }}
+                options={{
+                    title: "Settings",
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="settings-outline" color={color} size={size} />
+                    ),
+                }}
             />
         </Tabs>
     );
