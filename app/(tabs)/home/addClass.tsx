f1c3fde5abf67a524d52f0a91337bd150ce9d065
AanandShubham <www.subham.anand@gmail.com>
2025-10-20 03:43:29 +0530

    home designed  added

---
+++ app/(tabs)/home/addClass.tsx
@@ -0,0 +1,24 @@
+import InputBox from '@/app/components/InputBox'
+import React from 'react'
+import { View, Text } from 'react-native'
+import { Link } from 'expo-router'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import BaseContainer from '@/app/components/BaseContainer'
+const addClass = () => {
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView edges={['top', 'bottom']} className='w-full h-full bg-white '>
+              <BaseContainer headerLabel={"Add Class"} btnLabel={"Add"} >
+                <InputBox labelData={"Class Name"} />
+                <InputBox labelData={"Subject"} />
+                <InputBox labelData={"Time"} />   
+                <InputBox labelData={"Total Class"} />   
+                <InputBox labelData={"Room"} />   
+              </BaseContainer>
+            </SafeAreaView>
+        </SafeAreaProvider>
+
+    )
+}
+
+export default addClass
