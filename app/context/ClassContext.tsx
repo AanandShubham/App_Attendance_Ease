ce5cddae3164898469106d156ff7f15dc1ba43d3
AanandShubham <www.subham.anand@gmail.com>
2025-11-04 15:03:27 +0530

    class context  modified for attendance lIst

--- app/context/ClassContext.tsx
+++ app/context/ClassContext.tsx
@@ -11,16 +11,20 @@ type ClassContextType = {
     students: any,
     setStudents: (students: any) => void,
     selectedClass: any,
-    setSelectedClass: (selectedClasse: any) => void
+    setSelectedClass: (selectedClasse: any) => void,
+    attendanceList: any,
+    setAttendanceList: (attendanceList: any) => void
 }
 
 export const ClassContext = React.createContext<ClassContextType>({
     classes: [],
-    setClasses: (classes: any) => [],
+    setClasses: (classes: any) => {},
     students: [],
-    setStudents: (students: any) => [],
+    setStudents: (students: any) => {},
     selectedClass: {},
-    setSelectedClass: (selectedClass: any) => { }
+    setSelectedClass: (selectedClass: any) => { },
+    attendanceList: [],
+    setAttendanceList: (attendanceList: any) => { }
 })
 
 export const ClassContextProvider = ({ children }: { children: React.ReactNode }) => {
@@ -28,9 +32,10 @@ export const ClassContextProvider = ({ children }: { children: React.ReactNode }
     const [classes, setClasses] = useState<any>(null)
     const [selectedClass, setSelectedClass] = useState<any>(null)
     const [students, setStudents] = useState<any>(null)
+    const [attendanceList, setAttendanceList] = useState<any>(null)
 
     return (
-        <ClassContext.Provider value={{ classes, setClasses, selectedClass, setSelectedClass, students, setStudents }}>
+        <ClassContext.Provider value={{ classes, setClasses, selectedClass, setSelectedClass, students, setStudents,attendanceList,setAttendanceList }}>
             {children}
         </ClassContext.Provider>
     )
