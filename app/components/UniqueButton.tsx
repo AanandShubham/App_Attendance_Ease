a815afd9f41f3961618638229f0aa28fff991ed9
AanandShubham <www.subham.anand@gmail.com>
2025-12-01 22:36:55 +0530

    btn shake bug fixed

--- app/components/UniqueButton.tsx
+++ app/components/UniqueButton.tsx
@@ -1,57 +1,53 @@
-import React from 'react'
-import { Text, Pressable } from 'react-native'
-import { Animated } from 'react-native'
+import React, { useRef } from 'react'
+import { Text, Pressable, Animated } from 'react-native'
 import AddStudent from '@/assets/images/AddStudent.png'
 
-type uniqueButtonProps = {
-  btnStyleClass?: String,
-  itemStyleClass?: String,
-  label?: String,
-  btnAction?: () => void,
-  shakeAnim?: Animated.Value,
-  btnImageSource?: any,
+type UniqueButtonProps = {
+  btnStyleClass?: string
+  itemStyleClass?: string
+  label?: string
+  btnAction?: () => void
+  shakeAnim?: Animated.Value
+  btnImageSource?: any
 }
 
-const UniqueButton: React.FC<uniqueButtonProps> = (
-  {
-    btnStyleClass = "",
-    itemStyleClass,
-    label = "Click",
-    btnAction,
-    shakeAnim = new Animated.Value(0),
-    btnImageSource = "",
-  }
-) => {
+const UniqueButton: React.FC<UniqueButtonProps> = ({
+  btnStyleClass = "",
+  itemStyleClass = "",
+  label = "Click",
+  btnAction,
+  shakeAnim,
+  btnImageSource
+}) => {
+
+  // Create animation value only once
+  const internalShakeAnim = useRef(new Animated.Value(0)).current
+  const anim = shakeAnim || internalShakeAnim
 
   return (
     <Pressable
       onPress={btnAction}
-      className={`rounded-tr-[2px]  rounded-bl-[2px] rounded-tl-[48px] rounded-br-[48px] rounded-md flex justify-center items-center absolute  ${btnStyleClass}  dark:bg-[#183448] bg-[#3A87BD] shadow-2xl elevation-xl shadow-[#1B64A8]  border-2 dark:border-[#17242d] border-[#1B64A8]   absolute  `}
-     
+      className={`rounded-tr-[2px] rounded-bl-[2px] rounded-tl-[48px] rounded-br-[48px] rounded-md 
+        flex justify-center items-center absolute ${btnStyleClass}
+        dark:bg-[#183448] bg-[#3A87BD] shadow-2xl elevation-xl 
+        shadow-[#1B64A8] border-2 dark:border-[#17242d] border-[#1B64A8]`}
     >
-
-      {
-        btnImageSource ? <Animated.Image
+      {btnImageSource ? (
+        <Animated.Image
           source={btnImageSource || AddStudent}
           className={`w-[100%] h-[100%] p-2 ${itemStyleClass}`}
-        /> : <Animated.View
+        />
+      ) : (
+        <Animated.View
           style={{
-            transform: [{ translateY: shakeAnim }]
+            transform: [{ translateY: anim }]
           }}
         >
-          <Text className={`${itemStyleClass}`}>
-            {label}
-          </Text>
+          <Text className={itemStyleClass}>{label}</Text>
         </Animated.View>
-      }
-
-
-
-
-
+      )}
     </Pressable>
   )
-
 }
 
 export default UniqueButton
