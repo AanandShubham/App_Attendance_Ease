9c7201afbadd053a7955e9ca26563a90329ddcbb
AanandShubham <www.subham.anand@gmail.com>
2025-12-21 00:34:00 +0530

    update profile feature added

--- app/(tabs)/settings/profile.tsx
+++ app/(tabs)/settings/profile.tsx
@@ -1,12 +1,14 @@
 import React, { SetStateAction, useState } from 'react'
 import BaseContainer from '@/app/components/BaseContainer'
 import InputBox from '@/app/components/InputBox'
-import { Animated, View } from 'react-native'
+import { ActivityIndicator, Animated, Text, View } from 'react-native'
 import { useRef } from 'react'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import { RegisterTypeFormData } from '@/app/FromTypes'
 import ImageSelector from '@/app/components/ImageSelector'
 import useAuthContext from '@/app/context/AuthContext'
+import useUpdateUser from '@/app/hooks/useUpdateUser'
+import Toast from 'react-native-toast-message'
 
 type formData = {
     profile: string,
@@ -18,6 +20,8 @@ type formData = {
 }
 
 const Profile = () => {
+
+    const { loading, updateUser } = useUpdateUser()
 
     const { user } = useAuthContext()
     // console.log("---------------------------------------------")
@@ -69,10 +73,19 @@ const Profile = () => {
         ]).start();
     }
 
-    const btnControl = () => {
+    const btnControl = async () => {
         // console.log("Button Pressed")
         // console.log(fromData)
-        startShake()
+
+        if (!await updateUser(fromData, imageUrl)) {
+            startShake()
+        } else {
+            Toast.show({
+                type: "success",
+                text1: "Profile Updated Successfully"
+            })
+        }
+
     }
 
     return (
@@ -89,55 +102,60 @@ const Profile = () => {
 
                     {/* <Image className='w-[100px] h-[100px] bg-neutral-800 mb-2 mt-2' /> */}
 
-              
 
-                        <ImageSelector
-                            imgUrl={imageUrl}
-                            setImage={setImageUrl}
-                        />
-                        {/* labelData={"Class Name"}
+
+                    <ImageSelector
+                        imgUrl={imageUrl}
+                        setImage={setImageUrl}
+                    />
+                    {/* labelData={"Class Name"}
                         inputValue={selectedClass.name}
                         dataValue={formData.name}
                         setDataValue={text => handleInputChange("name", text)} */}
 
-                        <InputBox
-                            labelData={"fullname"}
-                            // inputValue={user?.fullname}
-                            dataValue={fromData.fullname}
-                            setDataValue={text => handleInputChange('fullname', text)}
-                        />
-                        <InputBox
-                            labelData={"username"}
-                            // inputValue={user?.username}
-                            dataValue={fromData.username}
-                            setDataValue={text => handleInputChange('username', text)}
-                        />
-                        <InputBox
-                            labelData={"security key"}
-                            // inputValue={user?.securityKey}
-                            dataValue={fromData.securityKey}
-                            setDataValue={text => handleInputChange('securityKey', text)}
-                            passwordMode={true}
-                        />
-                        <InputBox
-                            labelData={"password"}
-                            inputValue={"**********"}
-                            dataValue={fromData.password}
-                            setDataValue={text => handleInputChange('password', text)}
-                            passwordMode={true}
-                        />
+                    <InputBox
+                        labelData={"fullname"}
+                        // inputValue={user?.fullname}
+                        dataValue={fromData.fullname}
+                        setDataValue={text => handleInputChange('fullname', text)}
+                    />
+                    <InputBox
+                        labelData={"username"}
+                        // inputValue={user?.username}
+                        dataValue={fromData.username}
+                        setDataValue={text => handleInputChange('username', text)}
+                    />
+                    <InputBox
+                        labelData={"security key"}
+                        // inputValue={user?.securityKey}
+                        dataValue={fromData.securityKey}
+                        setDataValue={text => handleInputChange('securityKey', text)}
+                        passwordMode={true}
+                    />
+                    <InputBox
+                        labelData={"password"}
+                        // inputValue={"**********"}
+                        dataValue={fromData.password}
+                        setDataValue={text => handleInputChange('password', text)}
+                        passwordMode={true}
+                    />
 
-                        <InputBox
-                            inputValue={"**********"}
-                            labelData={"confirm password"}
-                            dataValue={fromData.confirmPassword}
-                            setDataValue={text => handleInputChange('confirmPassword', text)}
-                            passwordMode={true}
-                        />
-                   
-
+                    <InputBox
+                        // inputValue={"**********"}
+                        labelData={"confirm password"}
+                        dataValue={fromData.confirmPassword}
+                        setDataValue={text => handleInputChange('confirmPassword', text)}
+                        passwordMode={true}
+                    />
 
                 </BaseContainer>
+
+                {loading && (
+                    <View className='w-full h-full absolute bg-[#dae4e8e2] flex justify-center items-center gap-4'>
+                        <Text>Prosessing ...</Text>
+                        <ActivityIndicator />
+                    </View>
+                )}
             </SafeAreaView>
         </SafeAreaProvider>
     )
---
+++ app/hooks/useUpdateUser.ts
@@ -0,0 +1,156 @@
+import { useState } from "react"
+import Toast from "react-native-toast-message"
+import Constants from 'expo-constants'
+import useAuthContext from "../context/AuthContext"
+import { RegisterTypeFormData } from "../FromTypes"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+const useUpdateUser = () => {
+    const [loading, setLoading] = useState(false)
+    const { token, setUser, setToken } = useAuthContext()
+    const { apiUrl } = Constants.expoConfig?.extra || {}
+
+    const updateUser = async (registerForm: RegisterTypeFormData, imageUrl: string | null) => {
+
+        const flag = inputValidation(registerForm, imageUrl)
+
+        if (!flag) return false
+
+        setLoading(true)
+
+        try {
+
+            const formData = new FormData()
+            formData.append('username', registerForm.username)
+            formData.append('fullname', registerForm.fullname)
+            formData.append('password', registerForm.password)
+            formData.append('confirmPassword', registerForm.confirmPassword)
+            formData.append('securityKey', registerForm.securityKey)
+            // formData.append('profile',registerForm.profile as string)
+
+            if (imageUrl) {
+                formData.append('profile', {
+                    uri: imageUrl,
+                    type: "image/jpeg",
+                    name: "profile.jpg"
+                } as any)
+            }
+
+            const response = await fetch(`${apiUrl}/auth/update`, {
+                method: "POST",
+                body: formData,
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    "Authorization": `Token ${token}`
+                },
+            })
+
+            const data = await response.json()
+
+            if (data.error) throw new Error(data.error)
+
+            // console.log("----------------------------------------")
+            // console.log("update Data : ", data)
+            // console.log("----------------------------------------")
+
+            // setting and storing user  to the context and the async storage 
+            setUser(data.user)
+            await AsyncStorage.setItem("AuthUser", JSON.stringify(data.user))
+            setToken(data.token)
+            await AsyncStorage.setItem("AuthToken", JSON.stringify(data.token))
+
+            // setting the classes of the user to the class context 
+            // setClasses(data.user["classes"])
+
+            return true
+
+        } catch (error: any) {
+            Toast.show({
+                type: "error",
+                text1: `Error message : ${error.message}`
+            })
+            console.log("Error : ", error)
+        } finally {
+            setLoading(false)
+        }
+        return false
+    }
+
+    return { loading, updateUser }
+}
+
+
+export default useUpdateUser
+
+
+const inputValidation = (formData: RegisterTypeFormData, imageUrl: string | null) => {
+
+
+    if (
+        [
+            formData.fullname,
+            formData.username,
+            formData.password,
+            formData.confirmPassword,
+            formData.securityKey
+        ].some(item => item?.trim() === "")) {
+        Toast.show({
+            type: "error",
+            text1: " Fields Can't be Emplty !!!!!",
+            text1Style: {
+                backgroundColor: 'red',
+                padding: 4,
+                fontSize: 15,
+                color: "white"
+            },
+            text2: ""
+        })
+
+        return false
+    }
+
+    if (imageUrl == null) {
+        Toast.show({
+            type: "info",
+            text1: "Please Add Profile Photo ",
+            text1Style: {
+                backgroundColor: "yellow",
+                padding: 4,
+                color: "black"
+            },
+        })
+        return false
+    }
+
+    if (formData.password.length < 6) {
+        Toast.show({
+            type: "info",
+            text1: "Password Lenght Error ",
+            text1Style: {
+                backgroundColor: "yellow",
+                padding: 4,
+                color: "black"
+            },
+            text2: "! Password Should be greater than 6 ! "
+        })
+        return false
+    }
+
+    if (formData.password !== formData.confirmPassword) {
+        Toast.show({
+            type: "error",
+            text1: "! Password and ConfirmPassword should be same !",
+            text1Style: {
+                backgroundColor: 'red',
+                padding: 4,
+                fontSize: 15,
+                color: "white"
+            },
+            text2: ""
+        })
+
+        return false
+    }
+
+    return true
+}
